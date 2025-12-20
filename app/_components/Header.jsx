be419a25c1c3d4a23c178cf3b69cb953bd7116ae
Abdullah <abdulhhany69@gmail.com>
2025-12-20 14:46:11 +0200

    `Added @radix-ui/react-dropdown-menu and updated dependencies`

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -3,27 +3,51 @@
 import { Button } from "@/components/ui/button";
 import { LayoutGrid, Search, ShoppingBag } from "lucide-react";
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Api from "../_utils/Api";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Header = () => {
+  const [category, setCategory] = useState([]);
+
   useEffect(() => {
     getCategoryList();
   }, []);
+
   const getCategoryList = () => {
     Api.getCategory().then((res) => {
-      console.log(res.data.data);
+      setCategory(res.data.data);
     });
   };
+
   return (
     <div className="shadow-md flex justify-between p-2">
       <div className="flex items-center gap-8">
         <Image width={100} height={100} src="/logo.png" alt="logo" priority />
 
-        <h2 className="flex gap-2 items-center border p-2 rounded-full bg-slate-200">
-          <LayoutGrid />
-          Category
-        </h2>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <h2 className="cursor-pointer flex gap-2 items-center border p-2 rounded-full bg-slate-200">
+              <LayoutGrid />
+              Category
+            </h2>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Browse Category</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            {category.map((cat) => (
+              <DropdownMenuItem key={cat.id}>{cat.name}</DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+
         <div className="hidden md:flex gap-3 items-center border rounded-full p-2">
           <Search />
           <input type="text" placeholder="search" className="outline-none" />
