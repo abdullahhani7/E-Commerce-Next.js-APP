e2e3f2bd502b85e6abff1b087b24ba357385b4eb
Abdullah <abdulhhany69@gmail.com>
2025-12-21 11:10:48 +0200

    `Update CategoryList component: adjust grid gap and image sizes`

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -5,15 +5,15 @@ const CategoryList = ({ categoryList }) => {
   return (
     <div className="mt-5">
       <h2 className="font-bold text-2xl text-amber-500">Shop By Catgory</h2>
-      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6  justify-items-center items-center mt-5">
+      <div className="gap-10 md:gap-0 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6  justify-items-center items-center mt-5">
         {categoryList.map((cat, index) => (
-          <div key={index} className="flex flex-col items-center text-center  ">
+          <div key={index} className='w-30 h-35 mt-3  bg-amber-100  p-3 flex flex-col items-center text-center group'>
             <Image
               src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${cat?.icon?.[0].url}`}
-              width={50}
-              height={50}
+              width={70}
+              height={70}
               alt=""
-              className="rounded-full  hover:scale-125 transition-all "
+              className="rounded-full  hover:scale-125 transition-all cursor-pointer"
             />
             <p className="text-amber-500 font-bold capitalize">{cat?.name}</p>
           </div>
